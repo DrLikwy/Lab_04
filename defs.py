b7cdfa774d8e8e0b6a71619f2637c656131f14a8
Elisabeth <tretyak-li@yandex.ru>
2022-11-16 19:07:04 +0300

    Main part of lab_

---
+++ defs.py
@@ -0,0 +1,18 @@
+import string
+import random
+
+
+def key_maker(enter):  # генератор ключа
+    a = string.ascii_uppercase
+    block_1 = enter[3:6] + random_choice(a) + random_choice(a)
+    block_2 = enter[:3] + random_choice(a) + random_choice(a)
+    block_3 = str(int(enter[3:6]) + int(enter[:3]))
+    if len(block_3) == 3:
+        return str(block_1) + '-' + str(block_2) +' 0'+str(block_3)
+    if len(block_3) == 4:
+        return str(block_1) + '-' + str(block_2) +' '+str(block_3)
+
+
+def random_choice(a): # рандомайзер
+    b = random.choice(a)
+    return b
